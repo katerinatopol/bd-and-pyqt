eab4b58018f2467e0ccd17ee7affe7a9454d4d22
katerina_topol <katerina_topol@mail.ru>
2021-11-26 17:45:40 +0300

    upd task_3.py

--- task_3.py
+++ task_3.py
@@ -10,4 +10,38 @@ Unreachable
 10.0.0.3
 10.0.0.4
 """
+from tabulate import tabulate
 
+from task_1 import host_ping
+import ipaddress
+
+
+def host_range_ping_tab(range_cidr):
+    reachable = []
+    unreachable = []
+
+    try:
+        net = ipaddress.ip_network(range_cidr)
+        hosts = list(map(str, net.hosts()))
+        res = host_ping(hosts)
+
+    except ValueError as err:
+        print(f'Возникла ошибка: {err}')
+        return
+
+    for el in res:
+        if el[0] == 'Узел доступен':
+            reachable.append(el[2])
+        else:
+            if el[2] != 'Не определен':
+                unreachable.append(el[2])
+
+    all_word = ['Reachable', 'Unreachable']
+    sh_ip_int_br = [('\n'.join(reachable), '\n'.join(unreachable))]
+    print(tabulate(sh_ip_int_br, headers=all_word))
+
+    return res
+
+
+if __name__ == '__main__':
+    host_range_ping_tab('127.0.0.0/30')
